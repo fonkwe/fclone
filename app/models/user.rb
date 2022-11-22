0ebe73a03f45f002fe6f2018b688e7e50a00e398
fonkwe <williajeni@gmail.com>
2022-11-22 11:32:43 +0100

    authentication working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,47 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes, dependent: :destroy
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+ # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and 
+  # :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :friend_sent, class_name: 'Friendship',
+                         foreign_key: 'sent_by_id',
+                         inverse_of: 'sent_by',
+                         dependent: :destroy
+  has_many :friend_request, class_name: 'Friendship',
+                         foreign_key: 'sent_to_id',
+                         inverse_of: 'sent_to',
+                         dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) },
+           through: :friend_sent, source: :sent_to
+  has_many :pending_requests, -> { merge(Friendship.not_friends) },
+           through: :friend_sent, source: :sent_to
+  has_many :received_requests, -> { merge(Friendship.not_friends) },
+           through: :friend_request, source: :sent_by
+  has_many :notifications, dependent: :destroy
+ 
+  
+  # Returns a string containing this user's first name and last name
+  def full_name
+    "#{fname} #{lname}"
+  end
+  
+  # Returns all posts from this user's friends and self
+  def friends_and_own_posts
+    myfriends = friends
+    our_posts = []
+    myfriends.each do |f|
+      f.posts.each do |p|
+        our_posts << p
+      end
+    end
+    posts.each do |p|
+      our_posts << p
+    end
+    our_posts
+  end
+  
 end
